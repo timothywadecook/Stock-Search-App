04565e524c8f0acdf2d39ec3080ed6b246ba9e88
Timothy Cook <tim.wade.cook@gmail.com>
2019-02-01 17:06:47 -0500

    whitesmoke and dark gray design template made, addButtons working, validate input working,

--- app.js
+++ app.js
@@ -17,8 +17,10 @@
 
 
 // Initlization
-let stockList = [];
+let stockList = ['TSLA', 'AAPL', 'TWLO'];
 let validationList =[];
+
+console.log(stockList)
 
 // Asynchronously create validationList: array
 $.ajax({
@@ -28,7 +30,9 @@ $.ajax({
     response.forEach( function( object, i ) { 
         validationList.push( object.symbol )
     })
+    console.log( 'validation list = ', validationList )
 } );
+
 
 
 // checkInput Definition
@@ -37,7 +41,7 @@ const checkInput = function(e) {
     if ( $('#stock-input').val() == '' ) { alert( 'Please type a stock symbol first') }
     else {
         const symbol = $('#stock-input').val().trim().toUpperCase(); // store input value and trim and uppercase it
-        $('#stock-input').val() = ''; // reset input field to empty
+        $('#stock-input').val(); // reset input field to empty
         // check that symbol: string exists in the validation list 
         if ( validationList.includes(symbol) ) {
             updateStockList(symbol);
@@ -57,11 +61,15 @@ const updateStockList = function( symbol ) {
 const renderStockButtons = function ( stockList ) {
     let tempDiv = $('<div>');
     stockList.forEach( function(symbol, i) { 
-        let buttonHTML = 
+        let buttonHTML = `<button id='${symbol}' class='btn btn-primary mb-2 shadow stockBtn'>${symbol}</button>`
         tempDiv.append( buttonHTML );
     })
-}
+    $('#buttonList').empty();
+    $('#buttonList').append(tempDiv);
+    console.log( buttonList, tempDiv)
+};
 
+renderStockButtons( stockList )
 
 // *** create a renderStockButtons(stockList: array) function
 // Takes stockList: array 
